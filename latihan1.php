cdc33803bc4ab227bcd4872e9a378aa94787dd53
nadiapramesti <53283695+nadiapramesti@users.noreply.github.com>
2019-10-09 09:57:28 +0700

    Add files via upload

---
+++ latihan1.php
@@ -0,0 +1,47 @@
+<?php
+	/*
+	echo "HELLO WORD !";
+	echo "<h1>HELLO WORD !</h1>";
+	*/
+
+	/*
+	VARIABEL
+	diawali dengan tanda $nama
+	nama variabel tidak boleh diawali dengan angka
+	boleh mengandung angka
+	$nama = "Nadia Pramesti";
+	$pesan = "Halo, selamat datang <strong>$nama</strong>";
+	echo $pesan;
+	*/
+
+	/*
+	OPERATOR
+	Aritmatika
+	+ - * / % [modulus]
+	$a = 10;
+	$b = 20;
+	echo $a + $b;
+	*/
+	
+	/*
+	CONCATENNATION / CONCATC / PENGGABUNG STRING
+	.
+	$nama_depan = "Nadia";
+	$nama_belakang = "Pramesti";
+	echo $nama_depan . " " . $nama_belakang;
+	*/
+
+	/*
+	ASSIGMENT / PENGASAN
+	=, +=, -=, *=, /=, %=, .=
+	$x = "Nadia";
+	$x .= " ";
+	$x .= "pramesti";
+	echo $x;
+	echo "<br>";
+	$angka = 10;
+	$angka += 5;
+	$angka += 3;
+	echo $angka;
+	*/
+?>
